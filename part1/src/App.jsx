7d70559ddb3ba76727361da604124d469f89d424
luceraCalcio <luceracalcio2023@gmail.com>
2023-08-18 02:37:14 -0700

    props: passing data to components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,21 @@
 const Hello = (props) => {
+  console.log(props)
   
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name} , you are {props.age} years old</p>
     </div>
     );
 }
  
 const App = () => {
+  const name = "Peter"
+  const age = 10
   return ( 
     <div>
       <h1>Greatings </h1>
-      <Hello name="Bamba"/>
-      <Hello name="Kofi"/>
+      <Hello name="maya" age ={26-10}/>
+      <Hello name={name} age={age}/>
       
     </div>
    );
