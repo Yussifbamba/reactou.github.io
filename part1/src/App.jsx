53042d7dadd257382fc70a26e9fa7a234252d3e5
luceraCalcio <luceracalcio2023@gmail.com>
2023-08-18 02:21:25 -0700

    multi component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,21 @@
-const App=()=>{
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log (now , (a+b))
+const Hello = () => {
   return (
     <div>
-      <p>Hello World, it is {now.toString}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hello World</p>
     </div>
-  )
+    );
 }
+ 
+const App = () => {
+  return ( 
+    <div>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+    </div>
+   );
+}
+ 
+export default App;
 
-export default App  
+ 
